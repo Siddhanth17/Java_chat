3c22ca16032b5d1b6ce675395305e4acc039e627
Siddhanth <siddhanthdalal@gmail.com>
2026-04-13 14:24:03 +0530

    Client Handler Update

--- src/main/java/server/ClientHandler.java
+++ src/main/java/server/ClientHandler.java
@@ -1,21 +1,34 @@
 package server;
+import java.io.*;
+import java.net.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-
-public class ClientHandler extends Thread {
+public class ClientHandler extends Thread{
     private Socket socket;
     private BufferedReader in;
     private PrintWriter out;
     private String username;
 
-    public ClientHandler(Socket socket) throws IOException {
+    public ClientHandler(Socket socket) throws Exception{
         this.socket = socket;
         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         out = new PrintWriter(socket.getOutputStream(), true);
+    }
+
+    public void run(){
+        try{
+            String msg;
+
+            while((msg = in.readLine()) != null){
+                System.out.println("Received: " + msg);
+
+                String[] parts = msg.split(":", 3);
+                String type = parts[0];
+
+                if(type.equals("LOGIN")){
+                    username = parts[1];
+                }
+            }
+        }
     }
 
 }
